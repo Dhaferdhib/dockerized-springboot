05376f6b50f58093c343b832135b2abbaf1e342f
dhafer.dhib <dhibdhafer@gmail.com>
2018-03-08 12:15:17 +0100

    fixing configuration

--- docker-springboot-mysql/run.sh
+++ docker-springboot-mysql/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-while  ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
-    echo "Trying to connect to MySQL at ${DATABASE_PORT}..."
+while  ! exec 6<>/dev/tcp/database/3306; do
+    echo "Trying to connect to MySQL at ${MYSQL_PORT}..."
     sleep 10
 done
 
